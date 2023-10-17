3ba05b98b1b3165cd6d6cac2bc33fe6d0744e068
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2023-10-17 11:20:12 +0200

    All algorithms covered
    Not nice if some combination of operations used. But for a + b + c or a! + b! + c! is nice!
    Missing: nice output message

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import re
 import operator
+import math
 
 # resource: https://stackoverflow.com/a/47163546/2475919
 def plate_with_numbers(plate_case:str):
@@ -20,6 +21,12 @@ def plate_with_numbers(plate_case:str):
 def extract_numbers(full_plate:str):
     # returns a substring containing only numbers
     return re.search("[0-9]+", full_plate).group()
+
+
+def turn_to_ints(value_holder:list[str]):
+    # simple function accepts list of strings, returns list of integers
+    for number in value_holder:
+        yield int(number)
 
 
 # resource: https://stackoverflow.com/a/73521378/2475919
@@ -43,8 +50,20 @@ def generate_combinations(string_lenght, char_range):
     results = ["".join(result) for result in list(zip(*results))]
     return results
 
+
+# TODO: single operand operators
 # resource: https://stackoverflow.com/a/1740759/2475919
-ops = { "+": operator.add, "-": operator.sub } # etc.
+ops = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv
+} # etc.
+singular_ops = {
+    "!": math.factorial,
+    "^": lambda x: math.pow(x, 2),
+    "√": math.sqrt,
+}
 
 
 
@@ -53,15 +72,33 @@ def combine_algorithm(values:list[int], operators:list[str]):
         raise ValueError("Wrong number of inputs")
     for op_tuple in operators:
         first_op, second_op = list(op_tuple)
-        new_value = ops[first_op](values[0], values[1])
-        yield ops[second_op](new_value, values[2]), op_tuple
+        try:
+            new_value = ops[first_op](values[0], values[1])
+            final_value = ops[second_op](new_value, values[2])
+        except ZeroDivisionError:
+            # TODO: re-raise error
+            continue
+        yield final_value, op_tuple
 
 
-# source: https://stackoverflow.com/a/42619122/2475919
-def compare_operators_used(input_operators:list[str], ethalon:list[str]):
-    a = input_operators
-    b = ethalon
-    return (set(b) == set(a)  & set(b) and set(a) == set(a) & set(b))
+# def combine_singular(number:int, singular_operator:str):
+#     return singular_ops[singular_operator](number)
+
+
+
+def combine_singular_algorithm(values:list[int], operators:list[str]) -> list[int]:
+    if len(values) != 3 and len(operators) != 3:
+        raise ValueError("Wrong number of inputs")
+
+    for op_triple in operators:
+        new_val = []
+        for index, val in enumerate(values):
+            operation = op_triple[index]
+            # yield singular_ops[operation](val)
+
+            new_val.append(singular_ops[operation](val))
+        yield new_val, op_triple
+
 
 
 def init_args():
@@ -79,19 +116,33 @@ def init_args():
 if __name__ == "__main__":
     argsv = init_args()
     plates = filter(plate_with_numbers, argsv.plate_numbers)
-    to_int = lambda listing: [int(x) for x in listing]
-    # summ = lambda x, y, z: x+y+z
-    operator_combination = generate_combinations(2, ["+", "-"])
+    operator_combination = generate_combinations(2, ["+", "-", "*", "/"])
+    singular_operator_combination = generate_combinations(3, ["!", "^", "√"])
     for plate in plates:
         value = extract_numbers(plate).zfill(4)
-        input_values = to_int(list(value))
+        input_values = list(turn_to_ints(list(value)))
         output_value = input_values.pop(-1)
         for result, algorithm in combine_algorithm(input_values, operator_combination):
             if result == output_value:
-                if compare_operators_used(algorithm, ["+", "+"]):
+                if algorithm == "++":
                     print(plate, "plate is Dory's summation!")
-                elif compare_operators_used(algorithm, ["-", "-"]):
+                elif algorithm == "--":
                     print(plate, "plate is Dory's negation!")
+                elif algorithm == "**":
+                    print(plate, "plate is Dory's multiplication!")
                 else:
-                    print(plate, "plate is Dory's algorithm!")
-                break
+                    print(plate, "plate is in Dory's algorithms! Combination: ", algorithm)
+                # FIXME: if only first value accepted, uncomment next line
+                # break
+
+        for new_values, singular_algorithm in combine_singular_algorithm(input_values, singular_operator_combination):
+            for result, algorithm in combine_algorithm(new_values, operator_combination):
+                if result == output_value:
+                    if singular_algorithm == "!!!":
+                        print(plate, "plate is in Dory's factoriel algorithm!")
+                    elif singular_algorithm == "^^^":
+                        print(plate, "plate is in Dory's square algorithm!")
+                    elif singular_algorithm == "√√√":
+                        print(plate, "plate is in Dory's root algorithm!")
+                    else:
+                        print(plate, "plate is in Dory's algorithms! Combination: ", algorithm, singular_algorithm)
