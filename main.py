eacf082fce48cdf132d899dc9227da32c5ae8341
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2023-10-17 09:22:38 +0200

    Summ strategy
    First operation managed

--- main.py
+++ main.py
@@ -16,30 +16,9 @@ def plate_with_numbers(plate):
     plate_format = re.compile('^[a-zA-Z]{2}[0-9]{3,4}-[a-zA-z]{1,2}$')
     return True if plate_format.match(plate) is not None else False
 
+
 def extract_numbers(plate):
     return re.search("[0-9]+", plate).group()
-
-
-# resource: https://stackoverflow.com/a/73521378/2475919
-def getCharPower(stringLength, charRange):
-    charpowers = []
-    for x in range(0, stringLength):
-            charpowers.append(len(charRange)**(stringLength - x - 1))
-    return charpowers
-
-def Generator(stringLength, charRange):
-    workbench = []
-    results = []
-    charpowers = getCharPower(stringLength, charRange)
-    for x in range(0, stringLength):
-            while len(workbench) < len(charRange)**stringLength:
-                    for char in charRange:
-                            for z in range(0, charpowers[x]):
-                                    workbench.append(char)
-            results.append(workbench)
-            workbench = []
-    results = ["".join(result) for result in list(zip(*results))]
-    return results
 
 
 def init_args():
@@ -57,10 +36,11 @@ def init_args():
 if __name__ == "__main__":
     argsv = init_args()
     plates = filter(plate_with_numbers, argsv.plate_numbers)
+    to_int = lambda listing: [int(x) for x in listing]
+    summ = lambda x, y, z: x+y+z
     for plate in plates:
         value = extract_numbers(plate).zfill(4)
-        # commutative operations
-        # FIXME: 1 value, one time generated!
-        input = list(value)
-        input.pop(-1)
-        print(value, Generator(3, list(input)))
+        input = to_int(list(value))
+        output = input.pop(-1)
+        if summ(*input) == output:
+            print(plate, "plate is Dory's summation!")
